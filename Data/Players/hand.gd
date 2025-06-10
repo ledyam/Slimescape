cea7e9c1199d71eb216b2cdcab957027385e4777
Ledyam Ramos <82295064+ledyam@users.noreply.github.com>
2025-06-10 00:34:22 -0400

    mixed

--- Data/Players/hand.gd
+++ Data/Players/hand.gd
@@ -2,9 +2,5 @@ extends CharacterBody2D
 
 class_name MainCharacter
 
-var direction = PlayerDirections.new()
-var current_direction 
-
 const SPEED = 190.0
 const JUMP_VELOCITY = -350.0
-var is_moving = false 
