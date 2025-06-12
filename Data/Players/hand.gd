b759d7537a97d03c41fd16ebb94e5f0dc049f11e
Tu nombreBrian <alfonsosanchezbrian@gmail.com>
2025-06-12 02:11:40 -0400

    d

--- Data/Players/hand.gd
+++ Data/Players/hand.gd
@@ -13,6 +13,7 @@ var double_jump : bool = false
 func started_timer():
 	CentralSignal.play_movement.emit()
 
+<<<<<<< Updated upstream
 
 func _ready() -> void:
 	CentralSignal.double_jump.connect(on_double_jump)
@@ -21,3 +22,15 @@ func _ready() -> void:
 func on_double_jump():
 	double_jump = true
 	pass
+=======
+func tile_collision():
+	var tilemap : TileMapLayer = get_tree().get_first_node_in_group("tilemap")
+	if tilemap:
+		
+		var local_position = tilemap.to_local(global_position)
+		var tile_coords = tilemap.local_to_map(local_position)
+		var tile_data: TileData = tilemap.get_cell_tile_data( tile_coords)  # <-- Aquí agregamos el layer 0
+
+		if tile_data and tile_data.get_custom_data("es_pincho"):
+			ControlUi.reset_level()
+>>>>>>> Stashed changes
