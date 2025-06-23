b77dced344d77557658f133c58acc4c46572c15b
Ledyam Ramos <82295064+ledyam@users.noreply.github.com>
2025-06-22 21:58:27 -0400

    Update timer_bar
    Actualizada la barra de tiempo:
    
    Ahora la barra es centralizada en un solo nodo y no independiente

--- UI/Timers/timer_bar.gd
+++ UI/Timers/timer_bar.gd
@@ -1,6 +1,8 @@
-extends PanelContainer
+extends HBoxContainer
 
-@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var progress_time_bar: ProgressBar = $ProgressTimeBar
+@onready var progress_time_bar_2: ProgressBar = $ProgressTimeBar2
+
 
 func _ready():
 	LevelTimeManager.level_time_started.connect(_on_time_started)
@@ -9,17 +11,20 @@ func _ready():
 	LevelTimeManager.level_time_reset.connect(_on_time_ended)
 	
 func _on_time_started(max_time: float):
-	progress_bar.max_value = max_time
-	progress_bar.value = max_time
+	progress_time_bar.max_value = max_time
+	progress_time_bar.value = max_time
+	progress_time_bar_2.max_value = max_time
+	progress_time_bar_2.value = max_time
 
 
 func _on_time_changed(time_left: float , max_level_time):
-	progress_bar.value = time_left 
-
+	progress_time_bar.value = time_left 
+	progress_time_bar_2.value = time_left
 	# Cambiar color si el tiempo es crítico
 	if time_left < max_level_time * 0.2:
-		progress_bar.add_theme_color_override("font_color", Color.RED)
-
+		progress_time_bar.add_theme_color_override("font_color", Color.RED)
+		progress_time_bar_2.add_theme_color_override("font_color", Color.RED)
 
 func _on_time_ended():
-	progress_bar.value = progress_bar.max_value
+	progress_time_bar.value = progress_time_bar.max_value
+	progress_time_bar_2.value = progress_time_bar.max_value
